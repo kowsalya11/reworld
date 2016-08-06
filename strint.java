807edcf97965870f106e83c34e3e99f1e2302d5e
kowsalya11 <kowsalya996@gmail.com>
2016-08-06 17:18:52 +0530

    Add files via upload

---
+++ strint.java
@@ -0,0 +1,15 @@
+public class StringToIntegerconverter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        int c=Integer.parseInt(a);
+         int e=Integer.parseInt(b);
+        System.out.println(""+c);
+    }
+}
