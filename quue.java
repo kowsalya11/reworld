959845918c92130d6d8f792f72327b435adc06d5
kowsalya11 <kowsalya996@gmail.com>
2016-08-06 17:25:08 +0530

    Add files via upload

---
+++ quue.java
@@ -0,0 +1,28 @@
+public class Activity3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int b=s.length();
+        char[] a=new char[b];
+                 for(int i=0;i<b;i++)
+                 {
+          a[i]=s.charAt(i);
+                   }
+            
+        for(int i=0;i<b;i++)
+            {
+            for(int j=i+1;j<b;j++)
+            {
+                if(a[i]==a[j])
+                {
+                  
+                      
+            }
+        }
+            }}}
+    
+
